fa7ab9b95abbe3b5b86526b79d7d909ba7ef7017
JuanCarlos <juan.carlos.marques.pereira@gmail.com>
2026-02-08 21:57:48 -0300

    Implementação DELETE /transaction endpoint

--- src/main/java/com/graeff/transacao_api/controller/TransacaoController.java
+++ src/main/java/com/graeff/transacao_api/controller/TransacaoController.java
@@ -5,6 +5,7 @@ import com.graeff.transacao_api.business.services.TransacaoService;
 import com.graeff.transacao_api.controller.dtos.TransacaoRequestDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,4 +24,11 @@ public class TransacaoController {
         return  ResponseEntity.status(HttpStatus.CREATED).build();
 
     }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletarTransacoes(){
+        transacaoService.limparTransacoes();
+        return  ResponseEntity.ok().build();
+    }
+
 }
